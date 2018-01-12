f4fb2d0f2aa7e8c48d0ef391b36fcb8f04c8dfc9
johnking_ca <john.king.ca@gmail.com>
2018-01-12 13:09:29 -0500

    ci(docs-deploy.sh): check the output of travis git commit.

--- docs-deploy.sh
+++ docs-deploy.sh
@@ -13,4 +13,6 @@ git config user.email "github@travis-ci.org" && \
 git add . && \
 git commit -m "Deploy to GitHub Pages" && \
 
-git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+
+#git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
