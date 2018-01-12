7d512ebd5f325102e65974af0e04fee3fef55fdc
johnking_ca <john.king.ca@gmail.com>
2018-01-12 13:34:58 -0500

    chore(clean docs-deploy.sh):

--- docs-deploy.sh
+++ docs-deploy.sh
@@ -14,5 +14,3 @@ git add . && \
 git commit -m "Deploy to GitHub Pages" && \
 
 git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
-
-#git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
